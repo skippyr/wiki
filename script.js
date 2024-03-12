fe33f8d0c44f74ad604a75abceffa33b499c4834
Sherman Rofeman <skippyr.developer@gmail.com>
2024-03-12 17:01:25 -0300

    add navigation menu

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ let g_navigationMenuOptions = {
         "Functions": ["tdk_getWindowDimensions", "tdk_getCursorCoordinate", "tdk_isTTY"]
     }
 };
-let g_navigationMenu = document.querySelector(".navigation-menu");
 let g_tableOfContents = document.querySelector(".table-of-contents");
+let g_navigationMenu = document.querySelector(".navigation-menu");
 
 if (g_tableOfContents)
 {
@@ -19,4 +19,18 @@ if (g_tableOfContents)
         g_tableOfContents.innerHTML += `<li><a href="src/${key}/index.html">${key}</a></li>`;
     });
     g_tableOfContents.innerHTML += `</ul>`;
+}
+if (g_navigationMenu)
+{
+    g_navigationMenu.innerHTML += `<ul>`;
+    Object.keys(g_navigationMenuOptions[g_name]).forEach((key) => {
+        g_navigationMenu.innerHTML += `<li>${key}</li><ul>`;
+        g_navigationMenuOptions[g_name][key].forEach((subKey) => {
+            g_navigationMenu.innerHTML += Array.isArray(subKey)
+                ? `<li><a href="index.html#${subKey[1]}">${subKey[0]}</a></li>`
+                : `<li><a href="${subKey}.html">${subKey}</a></li>`;
+        });
+        g_navigationMenu.innerHTML += `</ul>`;
+    });
+    g_navigationMenu.innerHTML += `</ul>`;
 }
